2401a027ccfbed461edd97392eac38fbc9ca1e1c
fa.pastore <fa.pastore@reply.it>
2022-05-03 18:01:12 +0200

    primo committONE

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import styled from '@emotion/styled';
+import { Grid, Typography } from '@mui/material';
+import Container from '@mui/material/Container';
+import './styles/styles.css';
+
+const StyledContainer = styled(Container)(() => ({
+  paddingTop: '80px',
+  height: '100%',
+}));
+
+const HeaderGrid = styled(Grid)(() => ({
+  height: '80px',
+  backdropFilter: 'blur(2px)',
+  backgroundColor: 'rgba(0, 0, 0, 0.1)',
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%',
+  fontWeight: '500',
+  fontSize: '40px',
+  transition: 'all 100ms ease',
+  ':hover': {
+    fontSize: '48px',
+    fontWeight: '700',
+  },
+}));
+
+function App() {
+  return (
+    <StyledContainer>
+      <HeaderGrid container alignItems={'center'} justifyContent='center'>
+        <Grid item>React like a BOSS! 2K22</Grid>
+      </HeaderGrid>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+      <div>asdfasdfasdfa</div>
+    </StyledContainer>
+  );
+}
+
+export default App;
