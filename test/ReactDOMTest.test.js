13c65a78ed27c830d5fa36f64b3452bc8226cd79
r.carraro <r.carraro@reply.it>
2022-05-16 19:07:42 +0200

    Fix test

---
+++ test/ReactDOMTest.test.js
@@ -0,0 +1,10 @@
+import {render, screen} from '@testing-library/react'
+import App from '../src/App';
+
+test("PageTitle Test", async ()  => {
+  render(<div></div>);
+  screen.debug();
+ // const linkElement = screen.getByText(/learn react/i)
+
+  //screen.debug();
+});
--- test/RestTest.test.js
+++ test/RestTest.test.js
@@ -1,7 +1,6 @@
 import { getPokemonList } from "../src/services/pokemonService";
 
-it("Rest Test", () => {
-    getPokemonList().then((result) => {
-        console.log(result)
-    })
+it("Rest Test", async () => {
+    const pokemonList = await getPokemonList();
+    expect(pokemonList).toMatchSnapshot();
 });
