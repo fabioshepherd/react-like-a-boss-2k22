e92d8e871ce81d3feba74834b5ce8235ea7cfeb1
r.carraro <r.carraro@reply.it>
2022-06-03 02:19:06 +0200

    tests

--- test/ReactTestingLibrary.test.js
+++ test/ReactTestingLibrary.test.js
@@ -6,10 +6,12 @@ import "@testing-library/jest-dom/extend-expect";
 import PageTitle from "../src/components/common/PageTitle";
 import PokemonCard from "../src/components/Pokemon/PokemonCard";
 import PokemonPage from "../src/pages/PokemonPage";
-import { Router } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import { createMemoryHistory } from "history";
+import PokemonDetailPage from "../src/pages/PokemonDetailPage";
+import GifPage from "../src/pages/GifPage";
 
-describe("RTL TESTS", () => {
+describe("RTL TEST", () => {
   test("PageTitle Test", () => {
     render(<PageTitle title={"titolo pagina"} />);
 
@@ -29,7 +31,7 @@ describe("RTL TESTS", () => {
 
   // spiegazione slide !!
 
-  test("PokemonPage", async () => {
+  test("PokemonPage Test", async () => {
     // render(<PokemonPage  />);
 
     const history = createMemoryHistory();
@@ -57,10 +59,62 @@ describe("RTL TESTS", () => {
 
     expect(screen.getAllByTestId("pokemonCard").length).toBe(400);
 
+    // oppure
+    expect(screen.getAllByTestId("pokemonCard")).toHaveLength(400);
+
     // o in alternativa
 
     for (let i = 1; i <= 400; i++) {
       expect(screen.getByText(`${i}`)).toBeInTheDocument();
     }
   });
+
+  test("PokemonDetailPage Test", async () => {
+    const history = createMemoryHistory({ initialEntries: ["/pokemon/1"] });
+    render(
+      <Router location={history.location} navigator={history}>
+        <Routes>
+          <Route path="/pokemon/:number" element={ <PokemonDetailPage />}>
+          </Route>
+        </Routes>
+      </Router>
+    );
+
+    screen.debug();
+
+    expect(screen.getByText("Caricamento...")).toBeInTheDocument();
+
+    await screen.findByTestId("pokemonDetailPageImage");
+
+    screen.debug();
+
+    expect(screen.getByText("bulbasaur")).toBeInTheDocument();
+    expect(screen.getAllByTestId("frontBackPokemonImg")).toHaveLength(2)
+
+    //expect(document.querySelector("[data-testid='pokemonDetailPageImage']")).toBeInTheDocument();
+  });
+
+  test("FrontBackPokemonImg Test",() => {
+    // TODO
+    // BASED ON
+    expect(el).toHaveAttribute("src","src_a");
+
+  })
+
+
+  test("GifPage Test",async () => {
+    render(<GifPage />) 
+
+    screen.debug();
+
+    expect(screen.queryAllByTestId("gifElement")).toHaveLength(0);
+
+    await screen.findAllByTestId("gifElement")
+
+    expect(screen.queryAllByTestId("gifElement")).toHaveLength(50);
+
+    //TODO test inserimento dati dall'utnete e comparsa messaggi
+    // e
+    expect(aaa).toBeRequired();
+  })
 });
