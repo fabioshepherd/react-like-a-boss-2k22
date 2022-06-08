cde8e23bcb38263bf99c17d892e4331abee65431
r.carraro <r.carraro@reply.it>
2022-06-08 15:46:31 +0200

    rimossi snap

--- test/RestTest.test.js
+++
@@ -1,6 +0,0 @@
-import { getPokemonList } from "../src/services/pokemonService";
-
-it("Rest Test", async () => {
-    const pokemonList = await getPokemonList();
-    expect(pokemonList).toMatchSnapshot();
-});
