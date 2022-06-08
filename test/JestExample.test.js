11ec32e700d06d8db3827a0800d0d3a4094780b9
r.carraro <r.carraro@reply.it>
2022-06-08 13:07:43 +0200

    comples test

--- test/JestExample.test.js
+++ test/JestExample.test.js
@@ -30,10 +30,12 @@ describe("Pokemon Test", () => {
 
   // 4
   test("asObject function", () => {
-      console.log(bulbasaur.asObject())
+    //console.log(bulbasaur.asObject())
     expect(bulbasaur.asObject()).toBe({ name: "bulbasaur", hp: 100 }); // toBe --> toEqual
   });
 
+  // 5
+  // attack function calls playSound 5 times
   test("attack function", () => {
       // function spy
       const playSoundMocked = jest.spyOn(bulbasaur,"playSound");
@@ -43,7 +45,19 @@ describe("Pokemon Test", () => {
 
       // assertion
       expect(playSoundMocked).toHaveBeenCalledTimes(5)
-
-
   })
+
+  // 6
+  // problema getHp ritorna 100 ==> mockiamo il valore restituito
+  test("attack function for dead pokemon", () => {
+    // function spy
+    const getHpMocked =  jest.spyOn(bulbasaur,"getHp");
+    getHpMocked.mockReturnValue(0)
+
+    console.log("bulbasaur.getHp(): ", bulbasaur.getHp())
+
+
+    // assertion
+    expect(bulbasaur.attack).toThrow();
+})
 });
